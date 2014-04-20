3a7b1d1c5510daaaa89ef01d91560f37cce78151
Cleiton Gomes <cleitoncsg@gmail.com>
2014-04-20 18:48:41 -0300

    Adicionando exemplo de leitura e escrita em arquivos

---
+++ RoboVelc/Entrada e Saída em Arquivos/entrada e saida 01/main.hs
@@ -0,0 +1,19 @@
+import System.IO
+import Data.Char(toUpper)
+
+main :: IO ()
+main = do 
+       inh <- openFile "input.txt" ReadMode
+       outh <- openFile "output.txt" WriteMode
+       mainloop inh outh
+       hClose inh
+       hClose outh
+
+mainloop :: Handle -> Handle -> IO ()
+mainloop inh outh = 
+    do ineof <- hIsEOF inh --retorna True se o arquivo estiver vazio
+       if ineof
+           then return ()
+           else do inpStr <- hGetLine inh
+                   hPutStrLn outh (map toUpper inpStr)
+                   mainloop inh outh
