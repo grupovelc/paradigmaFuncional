b2bb4e568f2ff40cb3978551f3dd914b208d4a8a
Vanessa Barbosa <vanessabmdf@gmail.com>
2014-04-23 21:34:20 -0300

    Lendo Arquivos csv e adicionando em uma lista

---
+++ RoboVelc/lerArquivo.hs
@@ -0,0 +1,24 @@
+import System.IO
+import Control.Monad
+
+f :: [String] -> [Int]
+f = map read
+
+calc = do  
+        let list = []
+        handle <- openFile "tabela1Hora.csv" ReadMode
+        contents <- hGetContents handle
+        let singlewords = words contents
+            list = f singlewords
+
+        print list
+        hClose handle 
+
+
+
+----STACK OVERFLOW : LENDO UM ARQUIVO -----
+--getLines = liftM lines . readFile
+
+--main = do
+  --list <- getLines "arquivo.txt"
+    --mapM_ putStrLn list
