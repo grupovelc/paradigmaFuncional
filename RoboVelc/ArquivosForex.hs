2e8cba9d837d896c519156fafff8c43d67681522
Cleiton Gomes <cleitoncsg@gmail.com>
2014-04-27 12:20:42 -0300

    Finalizando o módulo com arquivos

---
+++ RoboVelc/ArquivosForex.hs
@@ -0,0 +1,34 @@
+--module ArquivosForex (lerCotacoes, lerQuantidadeCandles) where
+
+import System.IO
+--import Control.Monad
+import Foreign.Marshal
+
+stringFromFloat :: [String] -> [Float]
+stringFromFloat = map read
+
+obtemTipoGrafico = do
+    arquivoForex <- openFile "criterioEntrada.txt" ReadMode
+    contents <- hGetContents arquivoForex
+    let singlewords = words contents
+    let calda = tail singlewords
+    let tipoGrafico = head calda
+    return tipoGrafico
+
+lerCotacoes = do
+        let grafico = (unsafeLocalState obtemTipoGrafico) ++ ".csv"
+        print grafico
+        let listaDeCotacoes = []
+        arquivoForex <- openFile grafico ReadMode
+        conteudo <- hGetContents arquivoForex
+        let listaDePalavras = words conteudo
+            listaDeCotacoes = stringFromFloat listaDePalavras
+        return listaDeCotacoes
+
+lerCandle = do
+    arquivoForex <- openFile "criterioEntrada.txt" ReadMode
+    conteudo <- hGetContents arquivoForex
+    let listaDePalavras = words conteudo
+    let candle = truncate (read (last listaDePalavras) :: Float)
+    return candle
+
---
+++ RoboVelc/Main.hs
@@ -0,0 +1,10 @@
+import System.IO
+import Control.Monad
+import CorrelacaoDePearson
+
+stringFromFloat :: [String] -> [Float]
+stringFromFloat = map read
+
+main = do
+
+
